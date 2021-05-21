7bb677ada1a8262b1cdb53a3a1e879cb93a53196
Igor Krupenko <watchexpert2010@yandex.ru>
2021-05-21 16:50:35 +0300

    2105 fixes

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+let addButton = document.querySelector('.profile__editbutton');
+let popup = document.querySelector('.edit-form');
+let popup_close = document.querySelector('.edit-form__close-icon');
+function popup_active() {
+  popup.classList.add('edit-form_active');
+}
+function popup_inactive() {
+  popup.classList.remove('edit-form_active');
+}
+
+let nameInput = document.querySelector('.edit-form__field1');
+let jobInput = document.querySelector('.edit-form__field2');
+let submitButton = document.querySelector('.edit-form__submit-button');
+let title = document.querySelector('.profile__title');
+let subtitle = document.querySelector('.profile__subtitle');
+
+function submit() {
+  nameInput.textContent = nameInput.value;
+  jobInput.textContent = jobInput.value;
+  title.textContent = nameInput.value;
+  subtitle.textContent = jobInput.value;
+  popup.classList.remove('edit-form_active');
+}
+
+submitButton.addEventListener('click', submit);
+addButton.addEventListener('click', popup_active);
+popup_close.addEventListener('click', popup_inactive);
