dedc76097adbabebecae347bb4eee9fe29dafd21
Igor Krupenko <watchexpert2010@yandex.ru>
2021-05-28 15:41:49 +0300

    2805-1

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 let addButton = document.querySelector('.profile__editbutton');
 let popup = document.querySelector('.popup-wrapper');
 let popupClose = document.querySelector('.edit-form__close-icon');
-let nameInput = document.querySelector('.edit-form__input-line:first-child');
-let jobInput = document.querySelector('.edit-form__input-line:nth-of-type(2)');
+let nameInput = document.querySelector('.edit-form__input-line_name');
+let jobInput = document.querySelector('.edit-form__input-line_job');
 let submitForm = document.querySelector('.edit-form__input-form');
 let title = document.querySelector('.profile__title');
 let subtitle = document.querySelector('.profile__subtitle');
@@ -10,12 +10,9 @@ function popupActive() {
   popup.classList.add('popup-wrapper_active');
   nameInput.value = title.textContent;
   jobInput.value = subtitle.textContent;
-  nameInput.textContent = title.value;
-  jobInput.textContent = subtitle.value;
 }
 function popupInactive() {
   popup.classList.remove('popup-wrapper_active');
-
 }
 function submitInfo (evt) {
   evt.preventDefault();
